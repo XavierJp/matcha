9b4f10e569cbebb0f7c260b958d2ebcf53a0bde7
Kevin Barnoin <k.barnoin@gmail.com>
2021-11-16 16:07:05 +0100

    fix: console error message

--- ui/src/theme/components/icons/ChevronRight.jsx
+++ ui/src/theme/components/icons/ChevronRight.jsx
@@ -5,8 +5,8 @@ export function ChevronRight(props) {
   return (
     <Icon viewBox='0 0 6 10' {...props}>
       <path
-        fill-rule='evenodd'
-        clip-rule='evenodd'
+        fillRule='evenodd'
+        clipRule='evenodd'
         d='M3.78132 4.99828L0.481323 1.69828L1.42399 0.755615L5.66666 4.99828L1.42399 9.24095L0.481323 8.29828L3.78132 4.99828Z'
         fill='currentColor'
       />
