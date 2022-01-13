0307679c80d9c3d920960f77f482a6e8ac6636c2
Kevin Barnoin <k.barnoin@gmail.com>
2022-01-13 17:02:24 +0100

    feat: add icon

---
+++ ui/src/theme/components/icons/NavVerticalDots.jsx
@@ -0,0 +1,12 @@
+import { Icon } from '@chakra-ui/react'
+import React from 'react'
+
+export function MenuFill(props) {
+  return (
+    <Icon viewBox='0 0 4 14' width='4' height='14' {...props}>
+      <circle cx='2' cy='2' r='2' fill='#000091' />
+      <circle cx='2' cy='7' r='2' fill='#000091' />
+      <circle cx='2' cy='12' r='2' fill='#000091' />
+    </Icon>
+  )
+}
